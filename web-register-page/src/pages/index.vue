233d46ba1d3cc7270f834290673792ec7fba00a0
JuanDa90laq <42078371+JuanDa90laq@users.noreply.github.com>
2024-04-15 19:54:22 -0500

    feat: update index to use node service in cloud

--- web-register-page/src/pages/index.vue
+++ web-register-page/src/pages/index.vue
@@ -20,7 +20,7 @@
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/')
+      const response = await axios.get('https://webpageproyect.onrender.com/')
       data.value = response.data
     } catch (error) {
       console.error('error', error)
